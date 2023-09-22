912519d7520ba8897cef4a732a17a13618fee964
ElaineLeandro <elainerleandro1@gmail.com>
2023-09-22 14:06:51 -0300

    feature: implementação validação formulario lib Zod.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,38 @@
 import { Play } from 'phosphor-react'
 import { CountdownContainer, FormContainer, HomeContainer, Separator, StarCoutdownButton, TaskInput, MinutesAmountInput } from './stlyes'
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos')
+})
+
+interface NewCycleFormData{
+    task: string,
+    minutesAmount: number,
+}
+
 
 export function Home() {
+    const {register, handleSubmit, watch, formState} = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
+
+    function handleCreateNewCycle(data: NewCycleFormData){
+       console.log(data)
+    }
+
+    console.log(formState.errors)
+    const task = watch('task')
+    const  IsSubmitDisabled = !task;
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
@@ -12,6 +40,7 @@ export function Home() {
                         list='task-suggestions'
                         type="text"
                         placeholder='Dê um nome para o seu projeto'
+                        {...register('task')}
                     />
 
                     <datalist id="task-suggestions">
@@ -28,7 +57,8 @@ export function Home() {
                         placeholder='00' 
                         step={5}
                         min={5}
-                        max={60}
+                        // max={60}
+                        {...register('minutesAmount', {valueAsNumber:true})}
                         />
                         
 
@@ -48,7 +78,7 @@ export function Home() {
 
                 <StarCoutdownButton
                     type="submit"
-                    disabled
+                    disabled={IsSubmitDisabled}
                 >
                     <Play size={24} />
                     começar
