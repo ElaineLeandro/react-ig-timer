8065a753cdc1a73b423e0eebfc53f0d704c664cc
ElaineLeandro <elainerleandro1@gmail.com>
2023-09-25 13:57:05 -0300

    feature:  aplicando o hook useEffect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,96 @@
-import { Play } from 'phosphor-react'
-import { CountdownContainer, FormContainer, HomeContainer, Separator, StarCoutdownButton, TaskInput, MinutesAmountInput } from './stlyes'
-import {useForm} from 'react-hook-form'
-import {zodResolver} from '@hookform/resolvers/zod'
+import { HandPalm, Play } from 'phosphor-react'
+import { CountdownContainer, FormContainer, HomeContainer, Separator, TaskInput, MinutesAmountInput, StartCoutdownButton, StopCoutdownButton } from './stlyes'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Informe a tarefa'),
     minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
-    .max(60, 'O ciclo precisa ser de no máximo 60 minutos')
+        .number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos')
 })
 
-interface NewCycleFormData{
-    task: string,
-    minutesAmount: number,
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+    startDate: Date;
 }
 
 
 export function Home() {
-    const {register, handleSubmit, watch, formState} = useForm({
+
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+    const [amountSetcondsPassed, setAmountSecondsPassed] = useState(0)
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
     })
 
-    function handleCreateNewCycle(data: NewCycleFormData){
-       console.log(data)
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    useEffect(() => {
+        let interval: number
+        if (activeCycle) {
+            interval = setInterval(() => {
+                setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+            }, 1000)
+        }
+        return () => {
+            clearInterval(interval)
+        }
+    }, [activeCycle,])
+
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+            startDate: new Date(),
+        }
+
+        setCycles((state) => [...state, newCycle])
+        setActiveCycleId(id)
+        setAmountSecondsPassed(0)
+        reset()
     }
 
-    console.log(formState.errors)
+
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - amountSetcondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle])
+
     const task = watch('task')
-    const  IsSubmitDisabled = !task;
+    const IsSubmitDisabled = !task;
     return (
         <HomeContainer>
-            <form onSubmit={handleSubmit(handleCreateNewCycle)}action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
@@ -54,13 +112,13 @@ export function Home() {
                     <MinutesAmountInput
                         type='number'
                         id="minutesAmount"
-                        placeholder='00' 
+                        placeholder='00'
                         step={5}
                         min={5}
-                        // max={60}
-                        {...register('minutesAmount', {valueAsNumber:true})}
-                        />
-                        
+                        max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
+                    />
+
 
 
 
@@ -69,20 +127,29 @@ export function Home() {
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
-                <StarCoutdownButton
-                    type="submit"
-                    disabled={IsSubmitDisabled}
-                >
-                    <Play size={24} />
-                    começar
-                </StarCoutdownButton>
+                {activeCycle ? (
+                    <StopCoutdownButton
+                        type="button"
+                    >
+                        <HandPalm size={24} />
+                        Interromper
+                    </StopCoutdownButton>
+                ) : (
+                    <StartCoutdownButton
+                        type="submit"
+                        disabled={IsSubmitDisabled}
+                    >
+                        <Play size={24} />
+                        começar
+                    </StartCoutdownButton>
+                )}
 
             </form>
         </HomeContainer>
